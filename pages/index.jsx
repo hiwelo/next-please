0d03d23f7b85fde46d25aeb959fe93e9535cdbff
Damien Senger <damien@raccoon.studio>
2020-02-02 13:03:27 +0100

    Migrate start page to TypeScript

--- pages/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const StartScreen = () => 'Hello World';
-
-export default StartScreen;
---
+++ pages/index.tsx
@@ -0,0 +1,5 @@
+import React, { FunctionComponent } from 'react';
+
+const StartScreen: FunctionComponent = () => <>Hello World</>;
+
+export default StartScreen;
